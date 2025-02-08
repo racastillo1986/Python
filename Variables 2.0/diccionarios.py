a9e658a33a53c0977edf17d9a7a6159c62b64194
racastillo1986 <racastillo@coop15abril.fin.ec>
2025-02-08 15:52:42 -0500

    variables 2.0

---
+++ Variables 2.0/diccionarios.py
@@ -0,0 +1,6 @@
+#creando diccionario con dict()
+diccionario = dict(nombre = "ramiro", apellido = "castillo")
+
+#forma convencional como in json
+#diccionario = {'nombre': "javier", 'apellido': "mcfly"}
+print(diccionario)
