9f358076b47d5ed1609a408bde33d7ba233da08e
racastillo1986 <racastillo@coop15abril.fin.ec>
2025-02-08 16:06:41 -0500

    variables 2.0

--- Variables 2.0/diccionarios.py
+++ Variables 2.0/diccionarios.py
@@ -4,3 +4,25 @@ diccionario = dict(nombre = "ramiro", apellido = "castillo")
 #forma convencional como in json
 #diccionario = {'nombre': "javier", 'apellido': "mcfly"}
 print(diccionario)
+
+#Las tuplas pueden ser claves
+diccionario = {("yeye", "yeye2"):"jajaja"}
+print(diccionario)
+
+#Los conjuntos pueden ser claves con frozenset
+diccionario = {frozenset(["yeye", "yeye2"]):"jajaja"}
+print(diccionario)
+
+#creando diccionarios con fromkeys()
+#crea las claves con valor none
+diccionario = dict.fromkeys(["nombre", "apellido"])
+print(diccionario["nombre"])
+
+#otro caso pero con un string para que cree una clave por cada caracter
+#si le pongo un 2do parametro se lo asigna como valor a cada clave
+diccionario = dict.fromkeys("ABCD", "valor")
+print(diccionario)
+
+# otro caso
+diccionario = dict.fromkeys(["nombre", "apellido"], "valor")
+print(diccionario)
