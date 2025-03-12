a6f27283014d3b77adbaa0b1d750b5f6a9ca3a98
racastillo1986 <racastillo@coop15abril.fin.ec>
2025-03-12 16:55:35 -0500

    Modelo basico apiRest moure, endpoint get basico

--- FastApiMoureDev/main.py
+++ FastApiMoureDev/main.py
@@ -1,1 +1,12 @@
-print("hola")
+'''
+python -m uvicorn main:app --reload
+python -m -> indica que tome el uvicorn de donde este
+'''
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    print("endpoint")
+    return {"mensaje": "Hola brooo"}
