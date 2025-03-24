845fc7032679db96d8b90873e3828dc88a113582
racastillo1986 <racastillo@coop15abril.fin.ec>
2025-03-24 09:58:52 -0500

    Router product OK

--- FastApiMoureDev/main.py
+++ FastApiMoureDev/main.py
@@ -3,8 +3,12 @@ python -m uvicorn main:app --reload
 python -m -> indica que tome el uvicorn de donde este
 '''
 from fastapi import FastAPI
+from routers import products
 
 app = FastAPI()
+
+# routers
+app.include_router(products.router)
 
 @app.get("/")
 async def root():
